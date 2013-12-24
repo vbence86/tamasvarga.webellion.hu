ec0c58aa2052ff4afbca3f87dffde7def9eb2ce9
Bence Varga <bence@samson>
2013-12-24 00:01:29 +0000

    Content has been changed and extended

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,6 +6,15 @@
 			scrollTop = parseInt(offset && offset.top);
 		return $(document).scrollTop() - scrollTop;
 	}
+	function getTranslateCSSObject(str){
+		return {
+		  '-webkit-transform': 'translate('+str+')',
+		     '-moz-transform': 'translate('+str+')',
+		      '-ms-transform': 'translate('+str+')',
+		       '-o-transform': 'translate('+str+')',
+		          'transform': 'translate('+str+')'
+		}
+	}
 	$(document).ready(function(){
 		// Scroll to the menus
 		(function(){
@@ -16,21 +25,25 @@
 				}
 				var top = $(element).position().top;
 				$('body,html').animate({
-						scrollTop: top - 80
+						scrollTop: top - 25
 				}, 800, callback);
 			};
-			$('#menu-whyamihere, #carousel-whyamihere').click(function(e){
+			$('#menu-whyamihere, #carousel-button-2, #carousel-button-3').click(function(e){
 				e.preventDefault();
 				scroll_to('#whyamihere');
 			});
-			$('#menu-hire-me').click(function(e){
+			$('#menu-hire-me, #carousel-button-1').click(function(e){
 				e.preventDefault();
 				scroll_to('#hire-me');
 			});				
-			$('#menu-gallery').click(function(e){
+			$('#menu-gallery, #carousel-button-4').click(function(e){
 				e.preventDefault();
 				scroll_to('#gallery');
-			});			
+			});
+			$('#menu-prizes').click(function(e){
+				e.preventDefault();
+				scroll_to('#prizes');
+			});	
 			$('#menu-contact-me').click(function(e){
 				e.preventDefault();
 				scroll_to('#contact-me');
@@ -56,13 +69,7 @@
 				if (getScrollPosition($hire_me_text) < -500){
 					return;
 				}
-				$hire_me_text.css({
-					  '-webkit-transform': 'translate(0px)',
-					     '-moz-transform': 'translate(0px)',
-					      '-ms-transform': 'translate(0px)',
-					       '-o-transform': 'translate(0px)',
-					          'transform': 'translate(0px)'
-				});
+				$hire_me_text.css(getTranslateCSSObject('0px'));
 				hire_me_text_triggered = true;
 			}
 			$(window).scroll(function(){
@@ -70,6 +77,18 @@
 				fadeNavigator(scrollTop);
 				slideHireMeText(scrollTop);
 			});
+			// register WhyAreYouHere texts
+			(function(){
+				$('#whyamihere').find('.sliding-text-wrapper').each(function(){
+					var wrapper = this;
+					$(window).scroll(function(){
+						if (getScrollPosition(wrapper) < -500)
+							return;
+						$(wrapper).css(getTranslateCSSObject('0px'));
+						$(window).unbind('scroll', arguments.callee);
+					});				
+				});
+			})();		
 		})();
 	});
 })(this, this.document);
